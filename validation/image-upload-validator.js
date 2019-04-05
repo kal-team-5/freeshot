6768c1e383e3faeb7639e3af09dbd518d3634661
Chris Gutierrez <chris.n.gutierrez@gmail.com>
2019-04-05 10:16:17 -0700

    merged changes from master into branch

--- validation/image-upload-validator.js
+++ validation/image-upload-validator.js
@@ -5,14 +5,20 @@ module.exports = function validateImageInput(formInput) {
   let errors = {};
   //ImageUpload Form.
   formInput.url = !isEmpty(formInput.url) ? formInput.url : "";
-  
+  formInput.username = !isEmpty(formInput.username) ? formInput.username : "";
+
   if (Validator.isEmpty(formInput.url)) {
     errors.url = "Image URL is required";
   }
+
   if (!Validator.isEmpty(formInput.url)) {
     if (!Validator.isURL(formInput.url)) {
       errors.url = "Not a Valid URL";
     }
+  }
+
+  if (Validator.isEmpty(formInput.username)) {
+    errors.username = "Username is required";
   }
 
   return {
